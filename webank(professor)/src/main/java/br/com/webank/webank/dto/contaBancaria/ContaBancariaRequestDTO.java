768b15a88a96539a7fa9ceaf93ed4749f5b08ccd
GabrielGarcia02 <gabriielsimone06@gmail.com>
2023-10-15 16:56:46 -0300

    primeiro commit

---
+++ webank(professor)/src/main/java/br/com/webank/webank/dto/contaBancaria/ContaBancariaRequestDTO.java
@@ -0,0 +1,14 @@
+package br.com.webank.webank.dto.contaBancaria;
+
+public class ContaBancariaRequestDTO extends ContaBancariaBaseDTO{
+
+    private Long idTitular;
+
+    public Long getIdTitular() {
+        return idTitular;
+    }
+
+    public void setIdTitular(Long idTitular) {
+        this.idTitular = idTitular;
+    }    
+}
